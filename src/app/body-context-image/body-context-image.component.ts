0d0aa7925eb51f8f42708557a7de8c111ea8f29d
Shane Creedon <shane.creedon3@mail.dcu.ie>
2018-09-09 17:47:07 +0100

    Large update: - Navbar implemented design wise - Added video play background
    Signed-off-by: ShaneCreedon <shane.creedon3@mail.dcu.ie>

---
+++ src/app/body-context-image/body-context-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-context-image',
+  templateUrl: './body-context-image.component.html',
+  styleUrls: ['./body-context-image.component.css']
+})
+export class BodyContextImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
