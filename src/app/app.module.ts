c1721dcd2453550e208e2e35c96ff07e55c7e3d3
shaneCreedon <shane.creedon3@mail.dcu.ie>
2018-09-17 16:30:29 +0100

    Nav bar responsive
    Signed-off-by: ShaneCreedon <shane.creedon3@mail.dcu.ie>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
-import { BodyContextVideoComponent } from './components/body-context-video/body-context-video.component';
-import { BodyContextAboutMeComponent } from './components/body-context-about-me/body-context-about-me.component';
-import { BodyContextProjectsComponent } from './components/body-context-projects/body-context-projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationMenuComponent,
-    BodyContextVideoComponent,
-    BodyContextAboutMeComponent,
-    BodyContextProjectsComponent
   ],
   imports: [
     BrowserModule
