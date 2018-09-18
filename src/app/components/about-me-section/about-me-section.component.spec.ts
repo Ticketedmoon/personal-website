7776721808cdf821a6ce56cdd83a5326edc2a72c
shaneCreedon <shane.creedon3@mail.dcu.ie>
2018-09-18 15:29:28 +0100

    - Fully responsive about me section (not typed up yet)
    Signed-off-by: ShaneCreedon <shane.creedon3@mail.dcu.ie>

---
+++ src/app/components/about-me-section/about-me-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutMeSectionComponent } from './about-me-section.component';
+
+describe('AboutMeSectionComponent', () => {
+  let component: AboutMeSectionComponent;
+  let fixture: ComponentFixture<AboutMeSectionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AboutMeSectionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutMeSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
