30279a827ef31b9882639c6eea1200dc4faab45d
shaneCreedon <shane.creedon3@mail.dcu.ie>
2018-09-18 15:22:00 +0100

    - Fully responsive about me section (not typed up yet)
    Signed-off-by: ShaneCreedon <shane.creedon3@mail.dcu.ie>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
 import { IntroductionImageComponent } from './components/introduction-image/introduction-image.component';
+import { AboutMeSectionComponent } from './components/about-me-section/about-me-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationMenuComponent,
     IntroductionImageComponent,
+    AboutMeSectionComponent,
   ],
   imports: [
     BrowserModule
