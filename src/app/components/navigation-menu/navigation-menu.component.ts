2150d1cd24024a773bba1004f92ed448a5fdd4b1
shaneCreedon <shane.creedon3@mail.dcu.ie>
2018-09-23 18:25:32 +0100

    - About me section text complete
    Signed-off-by: ShaneCreedon <shane.creedon3@mail.dcu.ie>

--- src/app/components/navigation-menu/navigation-menu.component.ts
+++ src/app/components/navigation-menu/navigation-menu.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-navigation-menu',
-  templateUrl: './navigation-menu.component.html',
-  styleUrls: ['./navigation-menu.component.css']
+    selector: 'app-navigation-menu',
+    templateUrl: './navigation-menu.component.html',
+    styleUrls: ['./navigation-menu.component.css']
 })
 export class NavigationMenuComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    scroll(id) {
+        console.log(`scrolling to ${id}`);
+        const el = document.getElementById(id);
+        el.scrollIntoView();
+    }
 }
